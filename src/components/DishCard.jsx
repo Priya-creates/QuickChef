c47a828d43368d40ba0938fd599f331e83e6e7a9
Priya Nandwani <priyanandwani3@gmail.com>
2025-11-08 18:48:59 +0530

    Added favourite page

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -1,15 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { IoMdHeart } from "react-icons/io";
 
-const DishCard = (props) => {
+const DishCard = ({
+  id,
+  img,
+  name,
+  time,
+  favourites,
+  setFavourites,
+  isFavPage,
+}) => {
+  const [isFilled, setIsFilled] = useState(false);
+
+  function handleAddToFavorite(id, name, img, time) {
+    let favourites = JSON.parse(localStorage.getItem("favourites"));
+    if (!isFilled && !isPresent(id)) {
+      setIsFilled(true);
+
+      favourites = [
+        ...favourites,
+        { id: id, name: name, img: img, time: time },
+      ];
+      console.log("ADD: ", favourites);
+    } else {
+      setIsFilled(false);
+      favourites = favourites.filter((item) => item.id !== id);
+      console.log("REMOVE: ", favourites);
+    }
+    localStorage.setItem("favourites", JSON.stringify(favourites));
+    setFavourites(favourites);
+  }
+
+  function isPresent(idd) {
+    console.log("favv", favourites);
+    return favourites && favourites.some((item) => item.id === idd);
+  }
+
   return (
-    <div className="flex flex-col">
-      <div className="w-full md:w-[210px] lg:w-[250px] object-cover">
-        <img src={props.img} alt="meal-img" className="rounded shadow-md" />
+    <div className="flex flex-col bg-pink-100 py-3 px-3 rounded-sm">
+      <div className="w-full  object-cover">
+        <img src={img} alt="meal-img" className="rounded shadow-md" />
       </div>
-      <div className="text-[15px] text-center mt-4 font-semibold bg-pink-200 text-pink-700 rounded-md py-1 px-3 inline-block mx-auto">
-        {props.name}
+      <div className="flex justify-center items-center mt-5 gap-3 ">
+        <div className="p-1 px-3 text-[15px] text-center font-semibold bg-pink-100 text-pink-700 rounded-md  ">
+          {name}
+        </div>
+        <div
+          onClick={
+            !isFavPage
+              ? () => handleAddToFavorite(id, name, img, time)
+              : undefined
+          }
+          className={`flex justify-center items-center  ${
+            isFilled ? "animate-heart" : ""
+          } `}
+        >
+          {isPresent(id) || isFavPage ? (
+            <IoMdHeart size={20} className="text-pink-700" />
+          ) : (
+            <IoMdHeartEmpty size={20} className="text-pink-700" />
+          )}
+          
+        </div>
       </div>
-      <div className="text-center text-[14px] mt-2 text-pink-700">Time Required: {props.time} minutes</div>
+      <div className="text-center text-[14px] mt-2 text-pink-700">
+        Time Required: {time} minutes
+      </div>
     </div>
   );
 };
---
+++ src/components/Favourites.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { IoIosHeart } from "react-icons/io";
+import { RiArrowGoBackFill } from "react-icons/ri";
+import DishCard from "./DishCard.jsx";
+
+const Favourites = () => {
+  const [favourites, setFavourites] = useState([]);
+  const [isFavPage, setIsFavPage] = useState(true);
+
+  function setFav() {
+    let fav = JSON.parse(localStorage.getItem("favourites"));
+    setFavourites(fav);
+  }
+
+  useEffect(() => {
+    setFav();
+  }, []);
+
+  return (
+    <div className="px-6">
+      <div className="text-4xl mt-7 text-center text-pink-300 font-bold flex justify-center items-center gap-3 mb-10 ">
+        Your <span className=" font-bold text-pink-700">Loved</span> Picks{" "}
+        <IoIosHeart size={32} className="text-pink-700 inline" />
+      </div>
+      <div className="text-pink-700 mb-5 ">
+        <button className="flex gap-2 items-center p-2 px-4 rounded-md bg-pink-100 font-medium transition-all duration-300 ease-in-out hover:bg-pink-200 ">
+          <RiArrowGoBackFill />
+          <div>Go back</div>
+        </button>
+      </div>
+      {favourites.length === 0 && (
+        <div className="text-center text-gray-600  mt-[200px] ">
+          <div>No Favourites Added</div>
+        </div>
+      )}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-4 sm:gap-4 md:gap-5  place-items-center  mx-auto mb-10 ">
+        {favourites.length > 0 &&
+          favourites.map((item) => (
+            <div key={item.id}>
+              <DishCard
+                id={item.id}
+                name={item.name}
+                time={item.time}
+                img={item.img}
+                isFavPage={isFavPage}
+              />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Favourites;
